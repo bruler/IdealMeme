3ff8d85da14b26ef172d71edccf7b30af36b20c4
daemonSlayer <kpgupta98@gmail.com>
2017-01-14 01:26:16 +0530

    src/utils created

--- src/utils/c_print.py
+++ src/utils/c_print.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python
+__author__ = "Kapil Gupta"
+__copyright__ = "Copyright 2016, TheMallCloud"
+__credits__ = ["Kapil Gupta"]
+__license__ = "GPL"
+__version__ = "1.0.1"
+__maintainer__ = "Kapil Gupta"
+__email__ = "kpgupta98@gmail.com"
+__status__ = "Production"
+
 
 from __future__ import absolute_import
 from __future__ import print_function
