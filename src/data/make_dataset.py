1c9428e881ce76e50dd9c55a634545ff79d6fb1c
daemonSlayer <kpgupta98@gmail.com>
2017-01-14 01:25:00 +0530

    src/data update

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,5 +1,13 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/env python
+__author__ = "Kapil Gupta"
+__copyright__ = "Copyright 2016, TheMallCloud"
+__credits__ = ["Kapil Gupta"]
+__license__ = "GPL"
+__version__ = "1.0.1"
+__maintainer__ = "Kapil Gupta"
+__email__ = "kpgupta98@gmail.com"
+__status__ = "Production"
 
 
 import os
