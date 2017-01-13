d2d044a4e711af6d800400b68c4baaf0eff69874
daemonSlayer <kpgupta98@gmail.com>
2017-01-14 01:25:26 +0530

    src/models update

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python
+__author__ = "Kapil Gupta"
+__copyright__ = "Copyright 2016, TheMallCloud"
+__credits__ = ["Kapil Gupta"]
+__license__ = "GPL"
+__version__ = "1.0.1"
+__maintainer__ = "Kapil Gupta"
+__email__ = "kpgupta98@gmail.com"
+__status__ = "Production"
 
 from __future__ import absolute_import
 from __future__ import print_function
@@ -7,7 +15,5 @@ import sys
 sys.path.append('..')
 from visualization.visualize import visualize
 from utils.c_print import __ERR_SIG__
-
-
 
 
--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,1 +1,9 @@
 #!/usr/bin/env python
+__author__ = "Kapil Gupta"
+__copyright__ = "Copyright 2016, TheMallCloud"
+__credits__ = ["Kapil Gupta"]
+__license__ = "GPL"
+__version__ = "1.0.1"
+__maintainer__ = "Kapil Gupta"
+__email__ = "kpgupta98@gmail.com"
+__status__ = "Production"
