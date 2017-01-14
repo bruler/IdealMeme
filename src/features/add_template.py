ee949d161a9906c2b3087ab3c8189e0eb134a730
daemonSlayer <kpgupta98@gmail.com>
2017-01-14 13:29:27 +0530

    src/features update

--- src/features/add_template.py
+++ src/features/add_template.py
@@ -13,16 +13,34 @@ __email__ = "kpgupta98@gmail.com"
 __status__ = "Production"
 
 
-# import template_features_req_pb2
+import template_features_req_pb2
 import json
 import sys
 import os.path
 
 sys.path.append('..')
 path_to_json = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "_conf.json")
+path_to_book = os.path.join(os.path.dirname(os.path.abspath(__file__)), "parameters_book.prototxt")
 
 with open(path_to_json) as conf_file:
 	conf = json.load(conf_file)
 
+def CheckTemplatesNeeded(templates) :
+  pass
+
 def CreateTemplates(templates) :
-	pass
+	CheckTemplatesNeeded(templates)
+
+
+parameters_book = template_features_req_pb2.Templates()
+
+try:
+	with open(path_to_book, 'rb') as file:
+		parameters_book.ParseFromString(file.read())
+except IOError:
+	print('[!] Parameters Book not present, creating new book')
+
+CreateTemplates(parameters_book.templates.add())
+
+with open(path_to_book, 'wb') as file:
+	file.write(parameters_book.SerializeToString())
