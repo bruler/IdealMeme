eac3dee4b30b42b5a4eab3d1bd53cef6a768ee56
daemonSlayer <kpgupta98@gmail.com>
2017-01-14 13:31:07 +0530

    src/remotedb update

--- src/remotedb/cyclegeardb.py
+++ src/remotedb/cyclegeardb.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python
+
+from __future__ import absolute_import
+from __future__ import print_function
+
+
 __author__ = "Kapil Gupta"
 __copyright__ = "Copyright 2016, TheMallCloud"
 __credits__ = ["Kapil Gupta"]
@@ -8,11 +13,14 @@ __maintainer__ = "Kapil Gupta"
 __email__ = "kpgupta98@gmail.com"
 __status__ = "Production"
 
-from __future__ import print_function
-
 import psycopg2
 import pprint
 import csv
+import sys
+import os.path
+
+sys.path.append('..')
+path_to_csv = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "data",  "cyclegear.csv")
 
 connection_str = "dbname='cyclegear' port='5439' user='admin' password='Pragmas0ft' \
  host='mall-cloud-analyzer.cyaf02s2fx7z.us-east-1.redshift.amazonaws.com'"
@@ -30,7 +38,7 @@ all_rows = [row for row in rows]
 
 print(all_rows[0])
 
-with open('data.csv', 'w') as f :
+with open(path_to_csv, 'w') as f :
     writer = csv.writer(f, delimiter=',')
     column_names = [val[0] for val in cur.description]
     writer.writerow(column_names)  
