42d1083424c6c48fb177be96116c19c9ddcdd89f
daemonSlayer <kpgupta98@gmail.com>
2017-01-14 13:28:28 +0530

    src/models update

--- src/models/elastic_net_m.py
+++ src/models/elastic_net_m.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python
+
+from __future__ import absolute_import
+from __future__ import print_function
+
 __author__ = "Kapil Gupta"
 __copyright__ = "Copyright 2016, TheMallCloud"
 __credits__ = ["Kapil Gupta"]
@@ -7,13 +11,4 @@ __version__ = "1.0.1"
 __maintainer__ = "Kapil Gupta"
 __email__ = "kpgupta98@gmail.com"
 __status__ = "Production"
-
-from __future__ import absolute_import
-from __future__ import print_function
-import sys
-
-sys.path.append('..')
-from visualization.visualize import visualize
-from utils.c_print import __ERR_SIG__
-
 
