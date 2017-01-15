0e7e7fc032e72d04329bb3834fa977f37da45e3b
daemonSlayer <kpgupta98@gmail.com>
2017-01-15 16:01:45 +0530

    structure update + spark support

---
+++ src/core/spark/superlearner.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from __future__ import absolute_import
+from __future__ import print_function
+
+__author__ = "Kapil Gupta"
+__copyright__ = "Copyright 2016, TheMallCloud"
+__credits__ = ["Kapil Gupta"]
+__license__ = "GPL"
+__version__ = "1.0.1"
+__maintainer__ = "Kapil Gupta"
+__email__ = "kpgupta98@gmail.com"
+__status__ = "Production"
+
